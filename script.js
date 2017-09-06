a6b4249493fbc35375ff25afe02e0a8c79cf75be
mmcfarlin107 <mcfarlin107@gmail.com>
2017-09-06 19:53:33 -0400

    Cleaned up code and fixed basic error

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var userWord;
+  var userWord;
 
-//function to store the word the user needs to guess and create the empty letter spots
-$('#wordBtn').click(function(){
-  userWord = $('#userWord').val();
-  for(var i = 1; i <= userWord.length; i++){
-  $('#wordZone').append('<p class="letter" id="space' + i + '">  </p>');
-  console.log(userWord + ' ' + i);
-  $('#userWord').val('');
-}
+  //function to store the word the user needs to guess and create the empty letter spots
+  $('#wordBtn').click(function() {
+    userWord = $('#userWord').val();
+    for (var i = 1; i <= userWord.length; i++) {
+      $('#wordZone').append('<p class="letter" id="space' + i + '">  </p>');
+      console.log(userWord + ' ' + i);
+      $('#userWord').val('');
+    }
+  })
 
-$('#guessBtn').click(function(){
+  $('#guessBtn').click(function() {
 
-//clears the guess
-  $('#guessWord').val('');
-}
-})
+    //clears the guess
+    $('#guessWord').val('');
+  })
 
 
-//Closing out document.ready
+  //Closing out document.ready
 })
